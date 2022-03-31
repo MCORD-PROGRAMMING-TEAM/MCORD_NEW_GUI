57c02028e63484eb2bd97294622b76dd79d4a57a
MichalRK19780524 <michal.kiecana@gmail.com>
2022-03-31 12:24:45 +0200

    Improved formatting and requirements.txt

--- controller/controller.py
+++ controller/controller.py
@@ -3,12 +3,13 @@ from PySide6.QtCore import Qt
 from controller.usb_controller import USBController
 from controller.lan_controller import LanController
 
+
 class Controller:
-    def __init__(self,view,model) -> None:
+    def __init__(self, view, model) -> None:
         self._view = view
         self._model = model
-        self.usbcontroller = USBController(view,model)
-        self.lancontroller = LanController(view,model)
+        self.usbController = USBController(view, model)
+        self.lanController = LanController(view, model)
         self._constantSettings()
         self._connectToggle()
         self._applybuttonspage()
@@ -20,13 +21,7 @@ class Controller:
         self._connectionlogic()
         self._enable_shadow_effect()
         self._diagnosticlogic()
-  
-    
-        
-        
-        
-        
-        
+
     def _connectToggle(self):
         self._view.ui.ToggleButton.clicked.connect(self._view.toggleMenu)
         self._view.ui.SettingsButton.clicked.connect(self._view.toggleMenu)
@@ -35,18 +30,17 @@ class Controller:
         self._view.ui.closeButton.clicked.connect(self._view.close)
         self._view.ui.minimizeButton.clicked.connect(self._view.showMinimized)
         self._view.ui.usb_lan_button.stateChanged.connect(self._view.animated_changeUSB_IP)
-        self._view.ui.connection_combox.currentIndexChanged.connect(self.usbcontroller.set_current_device)
+        self._view.ui.connection_combox.currentIndexChanged.connect(self.usbController.set_current_device)
         self._view.ui.connection_combox.currentIndexChanged.connect(self._usb_logic)
-        self._view.ui.connection_edit.textChanged.connect(self.lancontroller.set_current_device)
-        self._view.ui.connection_edit.editingFinished.connect(self.lancontroller.allowed_only_lan)
+        self._view.ui.connection_edit.textChanged.connect(self.lanController.set_current_device)
+        self._view.ui.connection_edit.editingFinished.connect(self.lanController.allowed_only_lan)
         self._view.ui.connection_edit.editingFinished.connect(self._lan_logic)
         self._model.up_or_down_powersupply_progressBar_frame.connect(self._view.animated_ProgressBar_PowerSuply_frame)
         self._model.up_or_down_settings_progressBar_frame.connect(self._view.animated_ProgressBar_Settings_frame)
         self._model.up_or_down_connection_progressBar_frame.connect(self._view.animated_ProgressBar_Connection_frame)
         self._view.ui.simp_combo.currentIndexChanged.connect(self._model.get_current_simp_and_board)
         self._view.ui.board_combo.currentIndexChanged.connect(self._model.get_current_simp_and_board)
-     
-        
+
     def _constantSettings(self):
         self._view.ui.titleframe.mouseMoveEvent = self.moveWindow
         self._view.splashscreen.sp.main_frame.mouseMoveEvent = self._view.splashscreen.moveWindow
@@ -56,35 +50,30 @@ class Controller:
         self._model.set_voltage_asNumber(self._view.ui.Setting_frame)
         self._view.hide_frames()
         self._view.update_simp_comlist()
-        
+
     def _enable_shadow_effect(self):
-        self._view.enable_shadow_effect(self._view.ui.LeftMenuFrame,50,10,5,80)
-        self._view.enable_shadow_effect(self._view.ui.background,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.splashscreen,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.splashscreen.sp.progressBar,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.ui.select_conn_frame,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.ui.PowerSupply_frame,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.ui.Setting_frame,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.ui.Parameters_frame,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.ui.Console_frame,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.ui.powersupply_progressbar,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.ui.Settings_progess_bar,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.ui.connection_progressbar,10,5,5,80)
-        self._view.enable_shadow_effect(self._view.ui.tabWidget,30,0,0,80)
-        self._view.enable_shadow_effect(self._view.ui.Diagionstic_frame,30,0,0,80)
-     
-    
-  
+        self._view.enable_shadow_effect(self._view.ui.LeftMenuFrame, 50, 10, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.background, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.splashscreen, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.splashscreen.sp.progressBar, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.select_conn_frame, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.PowerSupply_frame, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.Setting_frame, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.Parameters_frame, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.Console_frame, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.powersupply_progressbar, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.Settings_progess_bar, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.connection_progressbar, 10, 5, 5, 80)
+        self._view.enable_shadow_effect(self._view.ui.tabWidget, 30, 0, 0, 80)
+        self._view.enable_shadow_effect(self._view.ui.Diagionstic_frame, 30, 0, 0, 80)
+
     def _connectionlogic(self):
         self._view.ui.connection_combox.currentIndexChanged.connect(self._view.Timers_start)
         self._view.ui.connection_combox.currentIndexChanged.connect(self._view.animated_ProgressBar_Connection_frame)
-    
-        
+
         self._view.ui.connection_edit.editingFinished.connect(self._view.Timers_start)
         self._view.ui.connection_edit.editingFinished.connect(self._view.animated_ProgressBar_Connection_frame)
-   
-        
-        
+
     def _powerbuttonslogic(self):
         for button, linedit in self._model.all_power_buttons.values():
             button.stateChanged.connect(self._view.Timers_start)
@@ -94,8 +83,7 @@ class Controller:
             button.stateChanged.connect(self._view.clear_board_comlist)
             button.stateChanged.connect(self._view.update_board_comlist)
             button.stateChanged.connect(self._view.remove_from_table)
-            
-    
+
     def _simpframesettingslogic(self):
         self._view.ui.board_combo.currentIndexChanged.connect(self._model.get_changed_board)
         self._view.ui.simp_combo.currentIndexChanged.connect(self._view.animated_voltage_panels)
@@ -109,68 +97,53 @@ class Controller:
         self._view.ui.settings_button.clicked.connect(lambda: self._view.ui.parameters_board_combo.setCurrentIndex(0))
         self._view.ui.settings_button.clicked.connect(self._view.update_progress_circ)
 
-
     def _parameterslogic(self):
         self._view.ui.parameters_board_combo.activated.connect(self._view.update_progress_circ)
-
 
     def _applybuttonspage(self):
         self._model.get_all_menu_buttons(self._view.ui.buttonsframe)
         for button in self._model.all_menu_buttons:
-            button.clicked.connect(partial(self._view.changePage,button))
-            
+            button.clicked.connect(partial(self._view.changePage, button))
+
     def _runtimer(self):
         self._view.splashscreen.timer.timeout.connect(self._view.splashscreen.progress)
         self._view.ui.powerbuttons_timer.timeout.connect(self._view.Progress_bars_update)
         self._view.ui.settings_timer.timeout.connect(self._view.Progress_bars_update)
         self._view.ui.connection_timer.timeout.connect(self._view.Progress_bars_update)
-        
-    
+
     def _frames_animation(self):
         self._model.up_or_down_connection_progressBar_frame.connect(self._view.expend_frames_PowerSupply)
         self._model.up_or_down_powersupply_progressBar_frame.connect(self._view.expend_frames_Settings)
         self._model.up_or_down_settings_progressBar_frame.connect(self._view.expend_frames_Parameters)
-        
+
     def _lan_logic(self):
-        self.lancontroller.create_lan_client()
+        self.lanController.create_lan_client()
         if self._model.valid_ip and self._model.connected_lan:
             for button, _ in self._model.all_power_buttons.values():
-                button.stateChanged.connect(self.lancontroller.lan_send_start)
-                button.stateChanged.connect(self.lancontroller.lan_update_stop)
-            self._view.ui.settings_button.clicked.connect(self.lancontroller.lan_send_voltage)
-            self._view.ui.settings_button.clicked.connect(self.lancontroller.lan_send_update)
-            self._view.ui.closeButton.clicked.connect(self.lancontroller.close_lan_client)
-        
-    
+                button.stateChanged.connect(self.lanController.lan_send_start)
+                button.stateChanged.connect(self.lanController.lan_update_stop)
+            self._view.ui.settings_button.clicked.connect(self.lanController.lan_send_voltage)
+            self._view.ui.settings_button.clicked.connect(self.lanController.lan_send_update)
+            self._view.ui.closeButton.clicked.connect(self.lanController.close_lan_client)
+
     def _usb_logic(self):
-        self.usbcontroller.create_usb_connect()
+        self.usbController.create_usb_connect()
         for button, _ in self._model.all_power_buttons.values():
-            button.stateChanged.connect(self.usbcontroller.usb_send_start)
-            button.stateChanged.connect(self.usbcontroller.usb_update_stop)
-            
-        self._view.ui.settings_button.clicked.connect(self.usbcontroller.usb_send_update)
-        self._view.ui.settings_button.clicked.connect(self.usbcontroller.usb_send_voltage)
-        self._view.ui.closeButton.clicked.connect(self.usbcontroller.close_usb_connect)
-        
+            button.stateChanged.connect(self.usbController.usb_send_start)
+            button.stateChanged.connect(self.usbController.usb_update_stop)
+
+        self._view.ui.settings_button.clicked.connect(self.usbController.usb_send_update)
+        self._view.ui.settings_button.clicked.connect(self.usbController.usb_send_voltage)
+        self._view.ui.closeButton.clicked.connect(self.usbController.close_usb_connect)
+
     def _diagnosticlogic(self):
-        self._view.ui.calibration_button.stateChanged.connect(self.lancontroller.lan_calibration)
-        self._view.ui.recalib_button.stateChanged.connect(self.lancontroller.lan_autorun)
+        self._view.ui.calibration_button.stateChanged.connect(self.lanController.lan_calibration)
+        self._view.ui.recalib_button.stateChanged.connect(self.lanController.lan_autorun)
 
-    
-
-        
-    def moveWindow(self,event):
-        if self._view.isMaximized(): 
+    def moveWindow(self, event):
+        if self._view.isMaximized():
             self._view.maximize_restore()
         if event.buttons() == Qt.LeftButton:
             self._view.move(self._view.pos() + event.globalPos() - self._view.dragPos)
             self._view.dragPos = event.globalPos()
             event.accept()
-    
-            
-  
-            
-        
-    
-        
-    
